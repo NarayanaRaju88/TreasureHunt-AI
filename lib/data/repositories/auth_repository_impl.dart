4ee179ce73f32b1e51e15af8ad1587d916ab2257
NarayanaRaju88 <nccl.narayana@gmail.com>
2026-07-30 22:48:06 +0530

    Update auth_repository_impl.dart

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -1,8 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 
+import '../../core/errors/app_exceptions.dart';
+import '../../core/services/fcm_service.dart';
+import '../../core/services/firebase_auth_service.dart';
+import '../../core/services/firestore_service.dart';
+import '../../core/services/hive_service.dart';
+import '../../domain/models/user_model.dart';
 import '../../domain/repositories/auth_repository.dart';
 
+/// Default authentication repository backed by:
+///
+/// - Firebase Authentication
+/// - Cloud Firestore
+/// - Hive local cache
+/// - Optional FCM service
 class AuthRepositoryImpl implements AuthRepository {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
