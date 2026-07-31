0202521442d39cdf912aac6d4b03c16d77e158ca
NarayanaRaju88 <nccl.narayana@gmail.com>
2026-07-31 22:01:11 +0530

    Update auth_repository.dart

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,10 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart' as fb;
-
-import '../../../core/errors/app_exceptions.dart';
-import '../../../core/services/fcm_service.dart';
-import '../../../core/services/firebase_auth_service.dart';
-import '../../../core/services/firestore_service.dart';
-import '../../../core/services/hive_service.dart';
 import 'package:ai_treasure_hunt/domain/models/user_model.dart';
 
 /// Contract for authentication and user profile management.
@@ -41,10 +34,3 @@ abstract class AuthRepository {
 
   Future<void> syncFcmToken();
 }
-
-/// Default authentication repository backed by:
-///
-/// - Firebase Authentication
-/// - Cloud Firestore
-/// - Hive local cache
-/// - Optional FCM service
