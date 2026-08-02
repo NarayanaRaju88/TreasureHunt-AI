ad54346b155d97d80b9aeb753da9d1684d79a9f5
NarayanaRaju88 <nccl.narayana@gmail.com>
2026-08-02 18:22:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/providers/service_providers.dart';
+import 'firebase_options.dart';
 import 'presentation/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final prefs = await SharedPreferences.getInstance();
 
