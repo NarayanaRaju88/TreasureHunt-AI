06dbdcfcb366d471d9cfdfd166f752e8cde29958
NarayanaRaju88 <nccl.narayana@gmail.com>
2026-08-02 11:45:37 +0530

    Refactor comments in HiveConfig initialization

--- lib/config/hive_config.dart
+++ lib/config/hive_config.dart
@@ -1,8 +1,5 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
 class HiveConfig {
   static Future<void> initializeHive() async {
-    // Register adapters for your models here later
-    // await Hive.openBox('settings');
+    // Register adapters for your models here later.
   }
 }
